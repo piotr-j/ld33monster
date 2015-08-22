e6f9d049e87e42049598ee65c3b84267fe6eecf4
PiotrJ <me@piotrjastrzebski.io>
2015-08-22 16:18:55 +0200

    set resolution

--- desktop/src/io/piotrjastrzebski/monster/desktop/DesktopLauncher.java
+++ desktop/src/io/piotrjastrzebski/monster/desktop/DesktopLauncher.java
@@ -3,12 +3,13 @@ package io.piotrjastrzebski.monster.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.piotrjastrzebski.monster.MonsterGame;
-import net.mostlyoriginal.game.GdxArtemisGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-
+		config.useHDPI = true;
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new MonsterGame(), config);
 	}
 }
